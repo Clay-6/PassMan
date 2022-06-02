af3bc5d2e8ea800bfe874e73f80b54a8a5c5b736
Clayton <claytonwambu@gmail.com>
2022-06-02 22:22:47 +0100

    Removed useless consts from `manager::errors`

--- src/manager/errors.rs
+++ src/manager/errors.rs
@@ -1,7 +1,3 @@
-pub const ENTRY_EXISTS: &str = "Entry already exists";
-pub const ENTRY_DOESNT_EXIST: &str = "Entry does not exist";
-pub const ENTRY_ID_OOB: &str = "Entry id out of bounds";
-
 #[derive(Debug, thiserror::Error)]
 pub enum ManagerError {
     #[error("Entry already exists")]
