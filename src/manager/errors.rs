373f1c6d0fdbb173b4b6d9c77a1fd5b7ac7737b0
Clayton <claytonwambu@gmail.com>
2022-06-02 22:14:54 +0100

    Added error type

--- src/manager/errors.rs
+++ src/manager/errors.rs
@@ -1,3 +1,13 @@
 pub const ENTRY_EXISTS: &str = "Entry already exists";
 pub const ENTRY_DOESNT_EXIST: &str = "Entry does not exist";
 pub const ENTRY_ID_OOB: &str = "Entry id out of bounds";
+
+#[derive(Debug, thiserror::Error)]
+pub enum ManagerError {
+    #[error("Entry already exists")]
+    EntryExists,
+    #[error("Entry does not exist")]
+    EntryDoesntExist,
+    #[error("Note ID out of bounds")]
+    NoteIdOOB,
+}
